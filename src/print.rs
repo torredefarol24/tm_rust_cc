4056b689cd3125a7dcf3d927fa7fb38dc9927a6c
Toufiq <burningraven06@gmail.com>
2019-02-23 12:32:35 +0600

    create cargo module, create some print statements

---
+++ src/print.rs
@@ -0,0 +1,15 @@
+pub fn run() {
+	//Printing
+	println!("Hey There.");
+	println!("Number: {}", 1);
+
+	// Formatting
+	println!("Message: {}", "Wow");
+	println!("{} comes from {}", "TFQ", "DHK");
+
+	//Positional Args
+	println!("{0} comes from {1} and {0} also likes to {2}", "TFQ", "DHK", "eat");
+
+	//Named Args
+	println!("{name} likes to eat {food}", name = "TFQ", food="bread");
+}
