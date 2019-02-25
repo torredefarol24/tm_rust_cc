f16b8927107cbfe6bcdb6f9122cf4966390092d5
Toufiq <burningraven06@gmail.com>
2019-02-26 01:12:50 +0600

    create conditionals module

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,10 +1,18 @@
 pub fn perform(){
-	let age = 18;
-	
-	if age >= 21 {
-		println!("Man : What biscuit would you like to eat?");
+	let age: u8 = 18;
+	let check_id : bool = true;
 
-	} else {
+	if age >= 21 && check_id {
+		println!("Man : What biscuit would you like to eat?");
+	} else if age < 21 && check_id {
 		println!("Man : You are too young!");
+	} else if age > 15 || check_id{
+		println!("Wow")
+	} else {
+		println!("Cool")
 	}
+
+	//let shorthand 
+	let is_of_age = if age >= 21 { true } else { false };
+	println!("Is of Age : {}", is_of_age);
 }
